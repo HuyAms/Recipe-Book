8949831246f283b85e11a236d5f9dc04ebb3a9e9
HuyTrinh <dinhhuyams@gmail.com>
2018-02-04 10:50:22 +0200

    adding child routing and configuring route parameters

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -10,17 +10,8 @@ import {RecipeService} from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
-  // @Output() recipeSelected = new EventEmitter<void>();
-
-  constructor(private recipeService: RecipeService) {
-  }
+  @Input() index: number;
 
   ngOnInit() {
   }
-
-  onSelected() {
-    // this.recipeSelected.emit();
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
-
 }
